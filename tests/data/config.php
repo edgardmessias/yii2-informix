453ebc5cd14a8416921f9ccf1fd3de943e08eab7
Edgard Lorraine Messias <edgardmessias@gmail.com>
2015-09-18 09:17:59 -0300

    Fixed connection config;

--- tests/data/config.php
+++ tests/data/config.php
@@ -8,9 +8,9 @@
 $config = [
     'databases' => [
         'informix' => [
-            'dsn'      => 'informix:host=127.0.0.1;service=9088;database=test;server=ol_informix1210;protocol=onsoctcp;CLIENT_LOCALE=en_US.utf8;DB_LOCALE=en_US.8859-1;EnableScrollableCursors=1',
-            'username' => 'informix',
-            'password' => 'informix',
+            'dsn'      => 'informix:host=127.0.0.1;service=9088;database=test;server=dev;protocol=onsoctcp;CLIENT_LOCALE=en_US.utf8;DB_LOCALE=en_US.utf8;EnableScrollableCursors=1',
+            'username' => 'test',
+            'password' => 'test',
         ]
     ],
 ];
