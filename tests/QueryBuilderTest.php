084ddf2bec94e3aa93399fedc5a4a311f883c576
Edgard Lorraine Messias <edgardmessias@gmail.com>
2015-09-30 09:40:38 -0300

    Fixed QueryBuilderTest;

--- tests/QueryBuilderTest.php
+++ tests/QueryBuilderTest.php
@@ -9,6 +9,26 @@ class QueryBuilderTest extends \yiiunit\framework\db\QueryBuilderTest
 {
 
     use DatabaseTestTrait;
+    use \yii\db\SchemaBuilderTrait;
 
     protected $driverName = 'informix';
+
+    /**
+     * @throws \Exception
+     * @return \edgardmessias\db\informix\QueryBuilder
+     */
+    protected function getQueryBuilder()
+    {
+        return new \edgardmessias\db\informix\QueryBuilder($this->getConnection(true, false));
+    }
+
+    /**
+     * adjust dbms specific escaping
+     * @param $sql
+     * @return mixed
+     */
+    protected function replaceQuotes($sql)
+    {
+        return str_replace('`', '', $sql);
+    }
 }
